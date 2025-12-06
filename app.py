7957df39f791f6f3b8ec67cde303b9daf1a91823
karthiktcs387 <kn262678@gmail.com>
2025-12-07 00:02:05 +0530

    Enable debug mode for Flask application
    Added debug mode to the Flask app.

--- app.py
+++ app.py
@@ -102,4 +102,8 @@ def delete_task(task_id):
 
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
+
     app.run(debug=True)
